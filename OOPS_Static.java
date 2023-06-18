cc2de10224d668bb3091689446b150b40d73539e
Ankush Paul <119006401+Ankush-Paul-02@users.noreply.github.com>
2023-06-18 21:25:49 +0530

    Update OOPS_Static.java

--- OOPS_Static.java
+++ OOPS_Static.java
@@ -1,5 +1,10 @@
-public class OOPS_Static {
+public class OOPS_STATIC {
+
+    /**
+     * If we make main static then we haven't made the Object of OOPS_STATIC
+     */
     public static void main(String[] args) {
+
        Mobile mobile1 = new Mobile();
        mobile1.brand = "Vivo";
        mobile1.price = 22000;
@@ -12,6 +17,8 @@ public class OOPS_Static {
 
        System.out.println(mobile1);
        System.out.println(mobile2);
+
+       Mobile.show(mobile1);
     }
 }
 
@@ -23,8 +30,34 @@ class Mobile {
      */
     static String name;
 
+    /**
+     * How many objects you are created will call a static block once
+     */
+    static {
+        name = "Phone";
+        System.out.println("In static block!");
+    }
+
+    public Mobile() {
+        brand = "";
+        price = 10000;
+        System.out.println("In constructor!");
+    }
+
     @Override
     public String toString() {
         return "Mobile [brand=" + brand + ", price=" + price + ", name=" + name + "]";
     }
+
+    /**
+     * Static method
+     *
+     * We can't directly use non-static variables in the static method, if we have an object
+     * reference then we can use it.
+     * @param mobile
+     */
+    public static void show(Mobile mobile) {
+        System.out.println("Mobile [brand=" + mobile.brand + ", price=" + mobile.price + ", name=" + mobile.name + "]");
+    }
 }
+
